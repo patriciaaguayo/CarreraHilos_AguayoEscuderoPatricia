bd441b773371b32601f5ba0e067ec7433de03411
patriciaaguayo <patriciaaguayop@gmail.com>
2024-11-25 13:33:06 +0100

    Creación del proyecto.

---
+++ CarreraHilos/src/main/java/com/mycompany/carrerahilos/CarreraHilos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carrerahilos;
+
+/**
+ *
+ * @author patriciaaguayo
+ */
+public class CarreraHilos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
